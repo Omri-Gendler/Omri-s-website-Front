9a295b978995fdc13eec249c9a8e2914f996a3e5
omri gendler <omrig999@gmail.com>
2025-11-06 01:18:29 +0200

    feat: Add preview configuration with host, port, and allowed hosts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	preview: {
+		host: '0.0.0.0',
+		port: process.env.PORT || 4173,
+		allowedHosts: ['omri-s-website-front.onrender.com']
+	},
 	// After setting-up a backend, this can automoate the copying process of the built files:
 	// build: {
 	// 	outDir: '../backend/public',
